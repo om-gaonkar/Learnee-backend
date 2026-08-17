f85a741d866955995ba1e2b66df22617bfda4288
om-gaonkar <omgaonkar999@gmail.com>
2026-08-18 01:22:19 +0530

    project initialized

---
+++ db/db.js
@@ -0,0 +1,43 @@
+import mongoose from "mongoose";
+
+let cached = global.mongoose;
+
+if (!cached) {
+  cached = global.mongoose = {
+    conn: null,
+    promise: null,
+  };
+}
+
+const connectDB = async () => {
+  console.log("connectDB() called");
+
+  if (cached.conn) {
+    console.log("Using cached MongoDB connection");
+    return cached.conn;
+  }
+
+  console.log("Attempting MongoDB connection...");
+  console.log("MONGODB_URI exists:", !!process.env.MONGODB_URI);
+
+  try {
+    cached.promise = mongoose.connect(process.env.MONGODB_URI, {
+      serverSelectionTimeoutMS: 5000,
+    });
+
+    cached.conn = await cached.promise;
+
+    console.log("MongoDB connected successfully");
+
+    return cached.conn;
+  } catch (error) {
+    cached.promise = null;
+
+    console.error("MongoDB connection failed:");
+    console.error(error);
+
+    throw error;
+  }
+};
+
+export default connectDB;
---
+++ server.js
@@ -0,0 +1,54 @@
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+import connectDB from "./db/db.js";
+import cookieParser from "cookie-parser";
+
+dotenv.config();
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use("/api", async (req, res, next) => {
+  try {
+    await connectDB();
+    next();
+  } catch (error) {
+    console.error("MongoDB connection failed:", error);
+
+    res.status(500).json({
+      message: "Database connection failed",
+    });
+  }
+});
+
+// ==========================Middleware==================================
+app.use(cookieParser());
+app.use(
+  cors({
+    origin: [
+      "http://localhost:5173",
+      "https://lost-and-found-iota-six.vercel.app",
+      "https://devtunnels.ms",
+      "https://zkx589fb-5173.inc1.devtunnels.ms",
+    ],
+    methods: ["GET", "POST", "DELETE", "PUT", "PATCH"],
+    allowedHeaders: [
+      "Content-Type",
+      "Authorization",
+      "Cache-Control",
+      "Expires",
+      "Pragma",
+    ],
+    credentials: true,
+  }),
+);
+app.use(express.json());
+
+// ===============================Route==========================================
+app.get("/", (req, res) => {
+  res.send("MERN Backend Server is running smoothly!");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is operating on port: ${PORT}`);
+});
